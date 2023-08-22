cdb45e69b56ac60c366f37887ace7296b6e3e726
matheus <de.leite2014@gmail.com>
2023-08-22 20:53:12 -0300

    feat: Created the style for DateInput component

--- src/Components/DateInput.tsx
+++ src/Components/DateInput.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React from "react";
 
-type IdateInput = React.ComponentProps<'input'> & {
+const generalStyle: React.CSSProperties = {
+  fontSize: "1rem",
+  color: "var(--color-2)",
+  padding: "var(--gap-s) .75rem",
+  background: "var(--color-4)",
+  borderRadius: "var(--gap)",
+};
+
+const labelStyles: React.CSSProperties = {
+  display: "block",
+  marginBottom: "var(--gap-s)",
+  fontWeight: "600",
+  ...generalStyle,
+};
+
+const inputStyle: React.CSSProperties = {
+  border: "none",
+  fontFamily: "monospace",
+  ...generalStyle,
+};
+
+type IdateInput = React.ComponentProps<"input"> & {
   label: string;
-}
+};
 
-const DateInput = ({label, ...props}: IdateInput) => {
+const DateInput = ({ label, ...props }: IdateInput) => {
   return (
     <div>
-      <label htmlFor={label}>{label}</label>
-      <input id={label} type="date" {...props}/>
+      <label style={labelStyles} htmlFor={label}>
+        {label}
+      </label>
+      <input style={inputStyle} id={label} type="date" {...props} />
     </div>
-  )
-}
+  );
+};
 
-export default DateInput
+export default DateInput;
