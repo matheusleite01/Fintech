3e67e638010be359abc8ad2e56ecda03d7042ea8
matheus <de.leite2014@gmail.com>
2023-08-24 22:51:15 -0300

    feat: Creating venda component

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,27 @@ import Resumo from "./Pages/Resumo";
 import Header from "./Components/Header";
 import Sidenav from "./Components/Sidenav";
 import { DataContextProvider } from "./Context/DataContext";
+import Vendas from "./Pages/Vendas";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Venda from "./Pages/Venda";
 
 const App = () => {
   return (
-    <DataContextProvider>
-      <div className="container">
-        <Resumo />
-        <main>
-          <Header />
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
           <Sidenav />
-        </main>
-      </div>
-    </DataContextProvider>
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Resumo />} />
+              <Route path="/vendas" element={<Vendas />} />
+              <Route path="/vendas/:id" element={<Venda />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   );
 };
 
--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { useData } from "../Context/DataContext";
 
 const Resumo = () => {
+  const { data } = useData();
+  if (data === null) return null;
   return (
-    <div>Resumo</div>
-  )
-}
+    <section>
+      <div className="resumo flex mb">
+        <div className="box">
+          <h2>vendas</h2>
+          <span>
+            {data
+              .filter((i) => i.status === "falha")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((i) => i.status === "pago")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((i) => i.status === "processando")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+      </div>
+      <div className="box mb">Graficos</div>
+    </section>
+  );
+};
 
-export default Resumo
+export default Resumo;
